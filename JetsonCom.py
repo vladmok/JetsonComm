1600910652250705036d06c9c0a12fe2fda12463
student <asd@school.com>
2018-02-20 13:47:57 -0500

    Updated code

--- JetsonCom.py
+++ JetsonCom.py
@@ -1,4 +1,36 @@
 import serial;
 
-ser = serial.Serial('COM5');
-print(ser.name);
+"""with serial.Serial('COM5', 115200) as ser:
+    x = ser.read();
+    x = ser.read(10);
+    line = 
+arduino = serial.Serial('COM6', 115200);"""
+
+def passByte(b):
+    print("Passing byte " + str(b))
+    
+def forward():
+    print("Forward")
+    passByte(0)
+
+def backward():
+    print("Backward")
+    passByte(1)
+
+def turn(d):
+    print("Turn in dir: " + str(d))
+    """ if d == 0 """
+    passByte(2)
+    """ else if d == 1 """
+    passByte(3)
+
+def main():
+    forward()
+    backward()
+    turn(1)
+
+if __name__ == "__main__":
+    main()
+    
+
+
