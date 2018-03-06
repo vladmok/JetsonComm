a848d191279da3c59ef700bd5a072b58129d55e9
Vlad <vladimirmokrushin@gmail.com>
2018-03-06 01:38:03 -0500

    Added movement and speed command serial communication

--- JetsonComm.ino
+++ JetsonComm.ino
@@ -1,15 +1,50 @@
 void setup() { 
   Serial.begin(9600);
   Serial.print("ARDUINO READY");
+  //daisy = Daisy(3,5,11);
 }
  
 void loop() {
-  int inByte = ' ';
-  while (!Serial.available()) {
-  }
-  inByte = Serial.read(); // read the incoming data
-  Serial.print("INPUT: ");
+
+  while (Serial.available() < 2) {
+  
+  int packet[2] = {Serial.read() , (Serial.read()<<8)}; // read the incoming data
+  char inByte = packet[0];
+  int inSpeed = packet[1];  
+  Serial.print("INPUT CHAR: ");
   Serial.println(inByte); // send the data back in a new line so that it is not all one long line
+  
+  Serial.print("INPUT SPEED: ");
+  Serial.println(inSpeed);
+  
+  if (inByte == 'm'){ // move forward
+
+    Serial.println("Forward");
+    }
+    
+  else if (inByte == 'l'){ //turn left
+
+    Serial.println("Left");
+    }
+    
+  else if (inByte == 'r'){ //turn right
+
+    Serial.println("Right");
+    }
+    
+  else if (inByte == 'b'){ // move backwards
+
+    }
+    
+  else if (inByte == 's'){ // stop
+
+    Serial.println("Stop");
+    }
+    
+  else
+    Serial.println("Error");
+    
+  }
 }
 
- 
+
---
+++ daisy_spine.py
@@ -0,0 +1,130 @@
+import serial
+from sys import argv
+
+class DaisySpine:
+    ser = None
+    def __init__(self, com_port = "/dev/ttyACM0", baud_rate = 28800, time_out = 1):
+        self.ser = serial.Serial(com_port, baud_rate, timeout = time_out)
+
+    def read_line(self):
+        print(self.ser.readline())
+
+    def read_all_lines_debug(self, chunk_size = 200):
+        print("=== READING ALL LINES ===")
+
+        read_buffer = b''
+
+        while True:
+            byte_chunk = self.ser.read(size = chunk_size)
+            read_buffer += byte_chunk
+            if not len(byte_chunk) == chunk_size:
+                break
+
+        print(read_buffer.decode("utf-8"))
+        self.ser.reset_input_buffer()
+
+        return read_buffer;
+
+        print("=== DONE ===")
+
+    def read_all_lines(self, chunk_size = 50):
+        read_buffer = b''
+
+        while True:
+            byte_chunk = self.ser.read(size = chunk_size)
+            read_buffer += byte_chunk
+            if not len(byte_chunk) == chunk_size:
+                break
+
+        self.ser.reset_input_buffer()
+
+        return read_buffer;
+
+    def pass_byte_basic(self, b):
+        self.ser.write(bytes([int(b)]))
+
+    def pass_byte_debug(self, b):
+        print("+++ PASSING BYTE +++")
+
+        self.ser.reset_input_buffer()
+        self.ser.reset_output_buffer()
+
+        if (int(b) > 255 or int(b) < 0):
+            print("Byte out of range: " + b)
+            print("+++ FAIL +++")
+            return
+
+        print("Passing byte " + str(b))
+        self.pass_byte_basic(b)
+
+        ret = self.read_all_lines()
+
+        self.ser.reset_input_buffer()
+        self.ser.reset_output_buffer()
+
+        print("+++ DONE +++")
+        return ret
+
+    def pass_byte(self, b):
+        self.ser.reset_input_buffer()
+        self.ser.reset_output_buffer()
+
+        if (int(b) > 255 or int(b) < 0):
+            print("Byte out of range: " + b)
+            return
+
+        self.pass_byte_basic(b)
+        ret = self.read_all_lines()
+
+        self.ser.reset_input_buffer()
+        self.ser.reset_output_buffer()
+
+        return ret
+
+#Test Movement Comm. Code: f = Forward, b = Backward, r = Right, l = Left, s = Stop
+    
+    speed = 200
+    movement = ' '
+    packet = bytearray([movement, speed])
+    def forward(self):
+        movement = 'f'
+        print("Forward")
+        self.pass_byte(1)
+
+    def backward(self):
+        movement = 'b'
+        print("Backward")
+        self.pass_byte(4)
+
+    def halt(self):
+        movement = 's'
+        print("Stopping")
+        self.pass_byte(0)
+
+    def turn(self, d):
+        print("Turning: " + str(d))
+
+        if d == 0:
+            movement = 'r'
+            self.pass_byte(2)
+        elif d == 1:
+            movement = 'l'
+            self.pass_byte(3)
+
+    ser.write(packet)
+    
+
+if __name__ == "__main__":
+    spine = None
+    if len(argv) != 1:
+        spine = DaisySpine(com_port = argv[1])
+    else:
+        spine = DaisySpine()
+
+    print(spine.read_all_lines())
+    spine.halt();
+    while True:
+        # spine.forward()
+        input_str = input()
+        if (len(input_str) > 0):
+            print(spine.pass_byte(input_str))
